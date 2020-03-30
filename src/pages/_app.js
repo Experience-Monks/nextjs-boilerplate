b414e4d79a2bdc18439089030d99ff6b70642f55
namniak <vnamnyak@gmail.com>
2020-03-30 13:36:58 -0400

    refactor: adding unsupported page

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,5 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import Head from 'next/head';
-import dynamic from 'next/dynamic';
 import 'normalize.css';
 import 'default-passive-events';
 
@@ -13,32 +12,18 @@ import detect, { isBrowser } from '../utils/detect';
 
 // This default export is required in a new `pages/_app.js` file.
 function App({ Component, pageProps }) {
-  const [isSupported, setIsSupported] = useState(true);
-
   useEffect(() => {
     if (isBrowser) {
-      const unsupportedUtil = require('../utils/unsupported-utils');
-      if (unsupportedUtil.isSupported()) {
-        if (process.env.NODE_ENV !== 'production' && window.location.href.indexOf('?nostat') === -1) {
-          require('@jam3/stats')();
-        }
-        const { device, browser } = detect;
-        const classes = [device.isMobile ? 'mobile' : '', device.getType(), browser.getName()].filter(className =>
-          Boolean(className)
-        );
-        document.body.className = [...document.body.className.split(' '), ...classes].filter(Boolean).join(' ');
-      } else {
-        setIsSupported(false);
+      if (process.env.NODE_ENV !== 'production' && window.location.href.indexOf('?nostat') === -1) {
+        require('@jam3/stats')();
       }
+      const { device, browser } = detect;
+      const classes = [device.isMobile ? 'mobile' : '', device.getType(), browser.getName()].filter(className =>
+        Boolean(className)
+      );
+      document.body.className = [...document.body.className.split(' '), ...classes].filter(Boolean).join(' ');
     }
   }, []);
-
-  if (!isSupported) {
-    const Unsupported = dynamic(() =>
-      import(/* webpackChunkName: "Unsupported" */ '../components/Unsupported/Unsupported')
-    );
-    return <Unsupported />;
-  }
 
   return (
     <Layout>
