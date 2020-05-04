5979c25f7c4655096247ffc50f913681aec9ef77
Wenchen Li <neo.li@jam3.com>
2020-05-04 17:37:14 -0400

    feature(typescript): convert the error page
    and utilize the type definitions from react-script

--- src/pages/_error.tsx
+++ src/pages/_error.tsx
@@ -1,11 +1,12 @@
 // Custom error page adapted from https://nextjs.org/docs/advanced-features/custom-error-page#customizing-the-error-page
 import React from 'react';
 import Head from 'next/head';
+import { NextPageContext, NextPage } from 'next';
 
 import Nav from '../components/Nav/Nav';
 import { ReactComponent as Cross } from '../assets/svgs/cross.svg';
 
-function Error({ statusCode }) {
+const Error: NextPage<{ statusCode: number }> = ({ statusCode }) => {
   return (
     <section className="Error">
       <Head>
@@ -18,11 +19,10 @@ function Error({ statusCode }) {
       <h1>{statusCode ? `An error ${statusCode} occurred on server` : 'An error occurred on client'}</h1>
     </section>
   );
-}
+};
 
-Error.getInitialProps = ({ res, err }) => {
-  const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
-  return { statusCode };
+Error.getInitialProps = ({ res, err }: NextPageContext) => {
+  return { statusCode: res?.statusCode ?? err?.statusCode ?? 404 };
 };
 
 export default Error;
