12b139a4b23e05b5c185b988a2ede56e400ab21c
Jerin Raisa <jerin.raisa11@gmail.com>
2021-03-16 11:12:32 -0400

    feature: updating lock body scroll util file

--- src/utils/lock-body-scroll.js
+++ src/utils/lock-body-scroll.js
@@ -1,43 +1,15 @@
-import { getScrollTop } from 'get-scroll';
-
-import scrollPage from './scroll-page';
-
 /**
- * Lock and unlock body scroll with page position restoration
+ * Lock and unlock body scroll within page
  */
-function lockBodyScroll() {
-  let scrollPosY = 0;
-  let isLocked = false;
 
-  /**
-   * Lock body scroll
-   */
-  function lock() {
-    if (!isLocked) {
-      scrollPosY = getScrollTop();
-      document.body.style.position = 'fixed';
-      document.body.style.overflowY = 'scroll';
-      document.body.style.marginTop = `-${scrollPosY}px`;
-      isLocked = true;
-    }
-  }
+ const root = document.documentElement;
 
-  /**
-   * Unlock body scroll
-   *
-   * @param {boolean} [skipPositionRestore=false] - Skip page position restoration flag
-   */
-  function unlock(skipPositionRestore = false) {
-    if (isLocked) {
-      document.body.style.position = '';
-      document.body.style.overflowY = '';
-      document.body.style.marginTop = '';
-      !skipPositionRestore && scrollPage({ y: scrollPosY });
-      isLocked = false;
-    }
-  }
-
-  return { isLocked, lock, unlock };
-}
-
-export default lockBodyScroll();
+ function lockBodyScroll() {
+   root.style.setProperty('--body-scroll', 'hidden');
+ }
+ 
+ function unlockBodyScroll() {
+   root.style.setProperty('--body-scroll', 'auto');
+ }
+ 
+ export { lockBodyScroll, unlockBodyScroll };
