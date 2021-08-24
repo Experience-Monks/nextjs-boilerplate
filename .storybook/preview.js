87adac6358e022bc9ccbf845609be75e63e28b43
donghyukjacobjang <donghyuk0117@gmail.com>
2021-08-24 15:02:58 -0400

    fix: add default redux provider to storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,17 @@
+import { Provider } from 'react-redux';
 import { RouterContext } from 'next/dist/shared/lib/router-context'; // next 11.2
 
 import '../src/styles/global.scss';
+
+import { store } from '../src/redux';
+
+export const decorators = [
+  (Story) => (
+    <Provider store={store}>
+      <Story />
+    </Provider>
+  )
+];
 
 export const parameters = {
   nextRouter: {
