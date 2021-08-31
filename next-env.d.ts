5f09882d7c1c06a7513de6410af7dcb6121d7b8d
donghyukjacobjang <donghyuk0117@gmail.com>
2021-08-31 10:41:10 -0400

    feature: add default 404 page

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,45 +1,5 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
 
-interface Window {
-  dataLayer: Array<object>;
-}
-
-type SetTimeout = ReturnType<typeof setTimeout>;
-
-declare module 'no-op' {
-  export default function noop(): void;
-}
-
-declare module 'get-scroll' {
-  export function getScrollTop(): number;
-}
-
-declare module '@jam3/react-hooks' {
-  export function useWindowSize(debounce: number): { innerWidth: number; innerHeight: number };
-}
-
-declare module '*.svg' {
-  const src: (props: React.SVGProps<SVGElement>) => React.ReactElement;
-  export default src;
-}
-
-declare module '*.png' {
-  const src: string;
-  export default src;
-}
-
-declare module '*.jpg' {
-  const src: string;
-  export default src;
-}
-
-declare module '*.jpeg' {
-  const src: string;
-  export default src;
-}
-
-declare module '*.gif' {
-  const src: string;
-  export default src;
-}
+// NOTE: This file should not be edited
+// see https://nextjs.org/docs/basic-features/typescript for more information.
---
+++ src/pages/404/index.tsx
@@ -0,0 +1,25 @@
+import { memo, useRef } from 'react';
+import classnames from 'classnames';
+
+import styles from './index.module.scss';
+
+import Head from '../../components/Head/Head';
+
+type Props = {
+  className: string;
+};
+
+function NotFound({ className }: Props) {
+  const containerRef = useRef<HTMLElement>(null);
+
+  return (
+    <main className={classnames(styles.NotFound, className)} ref={containerRef}>
+      <Head title="NotFound" />
+      <h1 className={styles.title}>NotFound Page</h1>
+    </main>
+  );
+}
+
+NotFound.defaultProps = {};
+
+export default memo(NotFound);
