590f7026e4f25ef3b51edfb256fe88597180e478
donghyukjacobjang <donghyuk0117@gmail.com>
2021-11-19 10:52:12 -0500

    fix: gtm script

--- src/utils/analytics.tsx
+++ src/utils/analytics.tsx
@@ -1,11 +1,6 @@
-import { memo } from 'react';
-import NextHead from 'next/head';
+import Script from 'next/script';
 
 if (typeof window !== 'undefined') window.dataLayer = window.dataLayer || [];
-
-type Props = {
-  consent: boolean;
-};
 
 /**
  * Dispatch an event with GTM
@@ -22,31 +17,28 @@ export function gtmEvent(action = false, payload = {}): void {
   }
 }
 
+type Props = {
+  consent: boolean;
+};
+
 /**
  * GTM code snippet in head
  */
-const GtmScript = () => {
-  return (
-    // TODO: next/head is not currently not accepting next/script component.
-    //       when it is fixed, use next/script
-    // eslint-disable-next-line
-    <script
-      id="gtm-container"
-      /* <!-- Google Tag Manager --> */
-      // eslint-disable-next-line
+export function GtmScript({ consent }: Props) {
+  return consent ? (
+    <Script
+      id="google-tag-manager"
+      strategy="afterInteractive"
+      // eslint-disable-next-line jam3/no-sanitizer-with-danger
       dangerouslySetInnerHTML={{
-        __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer', '${process.env.NEXT_PUBLIC_GTM_ID}');`
+        __html: `
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer', '${process.env.NEXT_PUBLIC_GTM_ID}');
+      `
       }}
     />
-  );
-};
-
-function Analytics({ consent }: Props) {
-  return <NextHead>{consent && GtmScript()}</NextHead>;
+  ) : null;
 }
-
-export default memo(Analytics);
