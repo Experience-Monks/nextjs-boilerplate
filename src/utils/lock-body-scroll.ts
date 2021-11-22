981b996cd2155d12a4ebbda5ca5299587c6b3e84
namniak <vnamnyak@gmail.com>
2021-11-22 11:33:55 -0500

    feature: update lock scroll

--- src/utils/lock-body-scroll.ts
+++ src/utils/lock-body-scroll.ts
@@ -1,43 +1,60 @@
 import { getScrollTop } from 'get-scroll';
+import noop from 'no-op';
 
-import scrollPage from './scroll-page';
+import scrollPage from '@/utils/scroll-page';
 
 /**
  * Lock and unlock body scroll with page position restoration
  */
 function lockBodyScroll() {
-  let scrollPosY = 0;
   let isLocked = false;
+  let lock = noop;
+  let unlock = noop;
 
-  /**
-   * Lock body scroll
-   */
-  function lock() {
-    if (!isLocked) {
-      scrollPosY = getScrollTop();
-      document.body.style.position = 'fixed';
-      document.body.style.overflowY = 'scroll';
-      document.body.style.marginTop = `-${scrollPosY}px`;
-      isLocked = true;
-    }
+  if (typeof window !== 'undefined') {
+    let scrollPosY = 0;
+
+    /**
+     * Lock body scroll
+     */
+    lock = function () {
+      if (!isLocked) {
+        scrollPosY = getScrollTop();
+        document.body.style.position = 'fixed';
+        document.body.style.overflowY = 'scroll';
+        document.body.style.marginTop = `-${scrollPosY}px`;
+        isLocked = true;
+      }
+    };
+
+    /**
+     * Unlock body scroll
+     * @param {boolean} [skipPositionRestore=false] - Skip page position restoration flag
+     */
+    unlock = function (skipPositionRestore = false) {
+      if (isLocked) {
+        document.body.style.position = '';
+        document.body.style.overflowY = '';
+        document.body.style.marginTop = '';
+        !skipPositionRestore && scrollPage({ y: scrollPosY });
+        isLocked = false;
+      }
+    };
   }
 
-  /**
-   * Unlock body scroll
-   *
-   * @param {boolean} [skipPositionRestore=false] - Skip page position restoration flag
-   */
-  function unlock(skipPositionRestore = false) {
-    if (isLocked) {
-      document.body.style.position = '';
-      document.body.style.overflowY = '';
-      document.body.style.marginTop = '';
-      !skipPositionRestore && scrollPage({ y: scrollPosY });
-      isLocked = false;
-    }
-  }
-
-  return { isLocked, lock, unlock };
+  return {
+    get isLocked() {
+      return isLocked;
+    },
+    lock,
+    unlock
+  };
 }
 
-export default lockBodyScroll();
+const instance = lockBodyScroll();
+
+if (typeof window !== 'undefined') {
+  Object.freeze(instance);
+}
+
+export default instance;
--- types.d.ts
+++ types.d.ts
@@ -5,7 +5,7 @@ interface Window {
 type SetTimeout = ReturnType<typeof setTimeout>;
 
 declare module 'no-op' {
-  export default function noop(): void;
+  export default function noop(...args): void;
 }
 
 declare module 'get-scroll' {
