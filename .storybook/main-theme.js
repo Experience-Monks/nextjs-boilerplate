563aac792e05e085247747d404f292614d8c55d7
alemesa <notalemesa@gmail.com>
2022-01-27 20:52:43 -0500

    chore: add brandImage to storybook theme

--- .storybook/main-theme.js
+++ .storybook/main-theme.js
@@ -2,10 +2,8 @@ import { create } from '@storybook/theming';
 
 export default create({
   base: 'light',
-
   brandTitle: 'Jam3 NextJS Boilerplate',
   brandUrl: 'https://jam3.com',
-  // TODO: add brandImage - logo
-
+  brandImage: '/favicons/favicon-32x32.png',
   colorSecondary: 'deepskyblue'
 });
