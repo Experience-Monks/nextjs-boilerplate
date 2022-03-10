dee560925b5f2c9331440cb07e3cefc6caafa2ff
Iran Reyes <iran.reyes@jam3.com>
2022-03-09 22:33:15 -0800

    feature: CircleCI new flow

---
+++ .circleci/scripts/deploy.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+
+#### Synchronizing with AWS ####
+cd out
+
+# Setup
+if [ "$CI_ENV" == "development" ]; then
+  export DELETE_OLD_FILES=--delete
+fi
+
+# Sync bundles with strong cache
+aws s3 sync ./favicons s3://${S3_ORIGIN_BUCKET}/favicons --metadata-directive 'REPLACE' --cache-control max-age=31536000,public ${DELETE_OLD_FILES}
+aws s3 sync ./_next s3://${S3_ORIGIN_BUCKET}/_next --metadata-directive 'REPLACE' --cache-control max-age=31536000,public ${DELETE_OLD_FILES}
+aws s3 sync ./assets s3://${S3_ORIGIN_BUCKET}/assets --metadata-directive 'REPLACE' --cache-control max-age=31536000,public ${DELETE_OLD_FILES}
+
+# Sync htmls and others with no cache
+aws s3 sync ./ s3://${S3_ORIGIN_BUCKET} --exclude "favicons/*" --exclude "_next/*" --exclude "assets/*" ${DELETE_OLD_FILES}
+
+aws configure set preview.cloudfront true
+
+INVALIDATION_ID=$(aws cloudfront create-invalidation \
+  --distribution-id $DISTRIBUTION_ID \
+  --paths '/*' | jq -r '.Invalidation.Id');
+
+aws cloudfront wait invalidation-completed \
+  --distribution-id $DISTRIBUTION_ID \
+  --id $INVALIDATION_ID
