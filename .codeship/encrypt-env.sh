c1e93e6fd59c3d2580edfed53def769ad3220fcf
Mike Nowak <mike.nowak@jam3.com>
2022-07-11 13:16:46 -0500

    ci: missing auto tagging; remove codeship

--- .codeship/encrypt-env.sh
+++
@@ -1,4 +0,0 @@
-jet encrypt .env.codeship.dev.local .env.codeship.dev.encrypted
-jet encrypt .env.codeship.stage.local .env.codeship.stage.encrypted
-jet encrypt .env.codeship.prod.local .env.codeship.prod.encrypted
-jet encrypt .env.codeship.ssh.local .env.codeship.ssh.encrypted
