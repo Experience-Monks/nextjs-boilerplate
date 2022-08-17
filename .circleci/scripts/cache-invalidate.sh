206b9f91201f169bdf80d6d48dbb6c5a0e13ce52
donghyukjacobjang <donghyuk0117@gmail.com>
2022-08-17 14:45:22 -0400

    fix: testing aws cache invalidate

--- .circleci/scripts/cache-invalidate.sh
+++ .circleci/scripts/cache-invalidate.sh
@@ -5,7 +5,7 @@ aws configure set preview.cloudfront true
 
 INVALIDATION_ID=$(aws cloudfront create-invalidation \
   --distribution-id $DISTRIBUTION_ID \
-  --paths '/*' | jq -r '.Invalidation.Id');
+  --paths '/*.html' '/assets*' '/favicons*' | jq -r '.Invalidation.Id');
 
 aws cloudfront wait invalidation-completed \
   --distribution-id $DISTRIBUTION_ID \
