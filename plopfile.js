fa1b2f964053a6ee3fcf3c6c653c7fcb72b069f4
Mike Nowak <mike.nowak@jam3.com>
2022-10-14 17:01:49 -0500

    feature: generator files for component

--- plopfile.js
+++ plopfile.js
@@ -17,16 +17,38 @@ module.exports = (plop) => {
       },
       {
         type: 'add',
-        path: './src/pages/{{name}}.tsx',
-        templateFile: 'scripts/templates/page/index.tsx.hbs'
+        path: './src/pages/index.module.scss',
+        templateFile: 'scripts/templates/page/index.module.scss.hbs'
       }
     ]
   });
 
   plop.setGenerator('component', {
-    description: 'Generator for a react page',
-    prompts: [], // array of inquirer prompts
-    actions: [] // array of actions
+    description: 'Generator for a react component',
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Your component name (use UpperCamelCase)'
+      }
+    ],
+    actions: [
+      {
+        type: 'add',
+        path: './src/components/{{name}}/{{name}}.tsx',
+        templateFile: 'scripts/templates/page/index.tsx.hbs'
+      },
+      {
+        type: 'add',
+        path: './src/components/{{name}}/{{name}}.module.scss',
+        templateFile: 'scripts/templates/page/index.tsx.hbs'
+      },
+      {
+        type: 'add',
+        path: './src/components/{{name}}/{{name}}.tsx',
+        templateFile: 'scripts/templates/page/index.tsx.hbs'
+      }
+    ]
   });
 
   plop.setGenerator('api', {
