a97d1cce0aac268357a6bf989e286cfc281ab85e
Muhammad Ahmed <mahmed.4zad@gmail.com>
2023-03-24 11:46:27 -0400

    feature: 'Google Tag Manager' set up respective to each environment
    Signed-off-by: Muhammad Ahmed <mahmed.4zad@gmail.com>

--- src/utils/analytics.tsx
+++ src/utils/analytics.tsx
@@ -27,6 +27,25 @@ type Props = {
 /**
  * GTM code snippet in head
  */
+/* const isDev =
+  typeof window !== 'undefined'
+    ? process.env.NODE_ENV === 'development' || window.location.href.includes('dev')
+    : false; */
+const isStaging = typeof window !== 'undefined' ? window.location.href.includes('stage') : false;
+const isProd = typeof window !== 'undefined' ? window.location.href.includes('prod') : false;
+
+let QUERY_PARAMS = '';
+if (isProd) {
+  // production env
+  QUERY_PARAMS = ``;
+} else if (isStaging) {
+  // staging env
+  QUERY_PARAMS = `+ '&gtm_auth=0C7oXHwDSkkVPTB79Nkerg&gtm_preview=env-4&gtm_cookies_win=x'`;
+} else {
+  // development && local env
+  QUERY_PARAMS = `+ '&gtm_auth=kCBpKoRDhvSr9u1LuPWC1g&gtm_preview=env-3&gtm_cookies_win=x'`;
+}
+
 export function GtmScript({ consent }: Props) {
   return consent ? (
     <Script
@@ -38,7 +57,7 @@ export function GtmScript({ consent }: Props) {
         (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl${QUERY_PARAMS};f.parentNode.insertBefore(j,f);
         })(window,document,'script','dataLayer', '${process.env.NEXT_PUBLIC_GTM_ID}');
       `
       }}
