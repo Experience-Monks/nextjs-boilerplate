9467841e2495fc3428081aa570c5c47191154e96
Muhammad Ahmed <mahmed.4zad@gmail.com>
2023-04-03 11:11:34 -0400

    fix: husky commit errors solved
    Signed-off-by: Muhammad Ahmed <mahmed.4zad@gmail.com>

--- src/utils/analytics.tsx
+++ src/utils/analytics.tsx
@@ -27,25 +27,10 @@ type Props = {
 /**
  * GTM code snippet in head
  */
-const isStage =
-  typeof window !== 'undefined' ? RegExp(/^https?:\/\/stage\..*$/).test(window.location.href.toLowerCase()) : false;
-const isProd =
-  typeof window !== 'undefined' ? RegExp(/^https?:\/\/prod\..*$/).test(window.location.href.toLowerCase()) : false;
-
-let QUERY_PARAMS = '';
-if (isProd) {
-  // production env
-  QUERY_PARAMS = ``;
-} else if (isStage) {
-  // staging env
-  QUERY_PARAMS = `+ '&gtm_auth=0C7oXHwDSkkVPTB79Nkerg&gtm_preview=env-4&gtm_cookies_win=x'`;
-} else {
-  // development && local env
-  QUERY_PARAMS = `+ '&gtm_auth=kCBpKoRDhvSr9u1LuPWC1g&gtm_preview=env-3&gtm_cookies_win=x'`;
-}
 
 export function GtmScript({ consent }: Props) {
-  return consent ? (
+  console.log(`Cookie consent: ${consent}`);
+  return (
     <Script
       id="google-tag-manager"
       strategy="afterInteractive"
@@ -55,10 +40,10 @@ export function GtmScript({ consent }: Props) {
         (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl${QUERY_PARAMS};f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer', '${process.env.NEXT_PUBLIC_GTM_ID}');
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl+ '&gtm_auth=kCBpKoRDhvSr9u1LuPWC1g&gtm_preview=env-3&gtm_cookies_win=x';f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer', 'GTM-K5TRM4N');
       `
       }}
     />
-  ) : null;
+  );
 }
