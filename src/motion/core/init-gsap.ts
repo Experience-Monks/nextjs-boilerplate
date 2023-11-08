c27b2dce0275df8133964102a0e9ac7afb01f1cc
Edgard Zavarezzi <edgardz@gmail.com>
2023-11-08 14:57:15 -0500

    chore: add commonly used MM eases

--- src/motion/core/init-gsap.ts
+++ src/motion/core/init-gsap.ts
@@ -1,7 +1,7 @@
 import { gsap } from 'gsap'
 import CustomEase from 'gsap/dist/CustomEase'
 
-import { customEases } from '../eases/eases'
+import { customEases, favouriteEases } from '../eases/eases'
 
 function initGsap() {
   if (typeof window === 'undefined') return
@@ -9,6 +9,10 @@ function initGsap() {
   gsap.registerPlugin(CustomEase)
 
   gsap.defaults({ ease: 'none', duration: 1 })
+
+  favouriteEases.forEach((ease) => {
+    CustomEase.create(ease.name, ease.ease)
+  })
 
   customEases.forEach((ease) => {
     CustomEase.create(ease.name, ease.ease)
